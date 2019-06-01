bd0c28caed8c0080d163de0b896dee6de5c86e57
beebeatle <ab2009ukr@gmail.com>
2019-06-01 17:49:06 -0400

    222

--- helloworld.py
+++ helloworld.py
@@ -5,7 +5,7 @@
 import stdio
 
 # Write 'Hello, World' to standard output.
-stdio.writeln('Hello, World!!!11qqqqqq')
+stdio.writeln('Hello, World!!!222')
 #33
 
 #-----------------------------------------------------------------------
